561d570e285f7783a139c0cf33aee3bad16ca7b6
Yuvraj <devyuvraj883@gmail.com>
2024-07-03 01:20:40 +0530

    Added link to the homepage

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,14 +1,20 @@
-//Shows login info about the user
-const UserInfo = ()=>{
-  return(
-   <div className="p-2 flex gap-2 border-2 border-gray-200 rounded-md">
-    <img src='/images/ChakrLogo.png' className="h-14 w-14 " alt="user-icon"/>
-    <div className="flex flex-col gap-1">
-      <h1 className="text-md font-bold">Yuvraj Singh</h1>
-      <p className="text-sm text-gray-400">yuvraj01@chakr.com</p>
-    </div>
+import Link from 'next/link'
 
-   </div>
+const UserInfo = () => {
+  return (
+    <Link href='/'>
+      <div className='p-2 flex gap-2 border-2 border-gray-200 rounded-md'>
+        <img
+          src='/images/ChakrLogo.png'
+          className='h-14 w-14 '
+          alt='user-icon'
+        />
+        <div className='flex flex-col gap-1'>
+          <h1 className='text-md font-bold'>Yuvraj Singh</h1>
+          <p className='text-sm text-gray-400'>yuvraj01@chakr.com</p>
+        </div>
+      </div>
+    </Link>
   )
 }
-export default UserInfo;
+export default UserInfo
